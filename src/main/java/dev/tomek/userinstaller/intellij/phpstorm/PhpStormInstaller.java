04677ff937d452fa0cc0cb958dcd6d4f1f18e4c6
Tomek Szmytka <tszmytka@gmail.com>
2022-03-20 17:30:22 +0100

    Use resolvable var for icons in phpstorm

--- src/main/java/dev/tomek/userinstaller/intellij/phpstorm/PhpStormInstaller.java
+++ src/main/java/dev/tomek/userinstaller/intellij/phpstorm/PhpStormInstaller.java
@@ -1,18 +1,27 @@
 package dev.tomek.userinstaller.intellij.phpstorm;
 
 import dev.tomek.userinstaller.action.Action;
+import dev.tomek.userinstaller.action.ResolveVars;
 import dev.tomek.userinstaller.intellij.IntellijInstaller;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Map;
 
 public class PhpStormInstaller extends IntellijInstaller {
+    private static final String PHPSTORM = "phpstorm";
+
     public PhpStormInstaller(String userHome, String settingsRepoUrl, String appsDir) {
-        super("phpstorm", userHome, settingsRepoUrl, appsDir);
+        super(PHPSTORM, userHome, settingsRepoUrl, appsDir);
     }
 
     @Override
     protected List<Action> buildCustomActions(Path homeDir, Path newInstallation) {
-        return List.of();
+        return List.of(
+            new ResolveVars(homeDir.resolve(Paths.get(PHPSTORM, "config", "settingsRepository", "repository")), Map.of(
+                "$applicationConfig", homeDir.resolve(Paths.get("idea", "config")).toString()
+            ))
+        );
     }
 }
